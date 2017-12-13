d49f9beee54184a580c728f25f0fa10004212a85
Hallee Palmer <pi.is.good.3.141@gmail.com>
2017-12-13 04:02:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ function viewCart() {
 if (cart.length==0){
   console.log("Your shopping cart is empty.")
 }
+else if (cart.length==1){
+  console.log("In your cart you have "+ cart[0][0] + "at $" cart[0][1])
+}
   // write your code here
 }
 
